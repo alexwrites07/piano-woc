3fe6b366a91e10541907b4a4e060c7bfee6c03fd
alexwrites07 <101046777+alexwrites07@users.noreply.github.com>
2022-04-16 16:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ function stopWatch(){
 
 
 
+
+
 function startStop(){
 
     if(status === "stopped"){
@@ -90,70 +92,3 @@ function reset(){
     document.getElementById("startStop").innerHTML = "Start";
 
 }
-
-const downloadLink = document.getElementById('download');
-const stopButton = document.getElementById('stop');
-
-class VoiceRecorder{
-    constructor(){
-        if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
-            console.log("Get user media supported");
-        }
-        else
-    {
-        console.log("Get user media NOT supported");
-    
-    }
-    this.mediaRecorder
-    this.stream
-    this.chunks =[]
-    this.isRecording = false
-    this.recordRef = document.querySelector("#recorder");
-    this.playerRef = document.querySelector("#player");
-    this.startRef = document.querySelector("#start");
-    this.stopRef = document.querySelector("#stop");
-        
-        this.startRef.onclick = this.startRecording.bind(this);
-        this.stopRef.onclick = this.stopRecording.bind(this);
-        this.constraints={
-            audio:true,
-            video: false
-        }
-    }
-    handleSuccess(stream){
-        this.stream = streamthis.stream.onactive = ()=>{
-            console.log("stream ended ");
-        }
-        this.recorderRef.scrObject =this.stream
-        this.mediaRecorder = new this.mediaRecorder(this.stream)
-        this.mediaRecorder.ondataavailable = this.onMediaRecorderDataAvailable.bind(this);
-        this.mediaRecorder.onstop = this.onMediaRecorderStop.bind(this);
-        this.recorderRef.play();
-        this.mediaRecorder.start();
-    }
-    onMediaRecorderDataAvailable(e){this.chunks.push(e.data)}
-    onMediaRecorderStop(e){
-        const blob=new Blob(this.chunks,{'type':'audio/ogg;codesc=opus'});
-        const audioURL = window.URL.createObjectURL(blob);
-        this.playerRef.src=audioURL;
-        this.chunks=[]
-        this.stream.getAudioTracks().forEach(track=>track.stop());
-        this.stream = null
-    }
-    startRecording(){
-        if(this.isRecording) return
-        this.isRecording=truethis.startRef.innerHTML="Recording...";
-        this.playerRef.src=' ';
-        navigator.mediaDevices.getUserMedia(this.constraints)
-        .then(this.handleSuccess.bind(this))
-        .catch(this.handleSuccess.bind(this))
-    }
-    stopRecording(){
-        if(!this.isRecording) return
-        this.isRecording=false
-        this.startRef.innerHTML="Record";
-        this.recorderRef.pause();
-        this.onMediaRecorder.stop()
-    }
-}
-window.VoiceRecorder = new VoiceRecorder();
